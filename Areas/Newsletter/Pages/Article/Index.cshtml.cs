d2730da946c0679883d9f26a10616d79538e67ca
augustofrade <aquaaticnaotrava@gmail.com>
2026-02-27 22:33:20 -0300

    Add article details and creation pages

---
+++ Areas/Newsletter/Pages/Article/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LittleFeed.Areas.Newsletter.Pages.Article;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Areas/Newsletter/Pages/Article/New.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LittleFeed.Areas.Newsletter.Pages.Article;
+
+public class New : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
