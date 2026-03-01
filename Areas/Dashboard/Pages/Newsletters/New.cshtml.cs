c1c734114a6042ffb6f8ac13a119e9b357bc8499
augustofrade <aquaaticnaotrava@gmail.com>
2026-03-01 14:25:05 -0300

    Add owner role generation for authenticated user when creating a Newsletter

--- Areas/Dashboard/Pages/Newsletters/New.cshtml.cs
+++ Areas/Dashboard/Pages/Newsletters/New.cshtml.cs
@@ -1,12 +1,16 @@
 using LittleFeed.Dto.Newsletters;
+using LittleFeed.Infrastructure.Identity;
 using LittleFeed.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LittleFeed.Areas.Dashboard.Pages.Newsletters;
 
-public class New(INewsletterService newsletterService, ILogger<New> logger) : PageModel
+public class New(INewsletterService newsletterService,
+    UserManager<ApplicationUser>  userManager,
+    ILogger<New> logger) : PageModel
 {
     [BindProperty]
     public CreateNewsletterDto Input { get; set; }
@@ -19,8 +23,11 @@ public class New(INewsletterService newsletterService, ILogger<New> logger) : Pa
     public async Task<IActionResult> OnPostAsync()
     {
         if (!ModelState.IsValid) return Page();
+        
+        var currentUserId = userManager.GetUserId(User);
+        
         logger.LogInformation("Received Newsletter POST request");
-        var newsletter = await newsletterService.CreateNewsletter(Input);
-        return RedirectToPage("Details",  new { slug = newsletter.Slug });
+        var newsletter = await newsletterService.CreateNewsletter(Input, currentUserId!);
+        return RedirectToPage("/Details",  new { area = "Newsletter", slug = newsletter.Slug });
     }
 }
